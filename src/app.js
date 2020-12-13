c3d20e4aa8fa7920d74437cb75aa3ca2591572ed
Laurel Bean <dawsonlaurel@gmail.com>
2020-12-13 15:58:57 -0600

    Initial app state completed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 import './app.css';
 
 class App extends Component {
@@ -11,4 +12,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default hot(module)(App);
